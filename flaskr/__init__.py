71c07c157c11b312a14842e949f2f4432e9b2706
lamine <mlamine2018@gmail.com>
2019-06-30 00:32:39 +0000

    Finalize the application coding

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,8 +1,13 @@
 from .flaskr import app, get_db
 
 def create_app():
+    
     from . import auth
+    from . import blog
+
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
     return app
 
 
